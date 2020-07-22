179a5386625f323592d010af720c2223e6802b74
Liz Lam <lizlam.217@gmail.com>
2020-07-21 23:50:16 -0700

    Add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 const tailwindcss = require("tailwindcss");
 
 module.exports = {
-  plugins: [tailwindcss("./tailwind.config.js")],
+  plugins:
+    [tailwindcss("./tailwind.config.js"),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/**/*.svelte',
+        './src/template.html'
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+    ],
 }
